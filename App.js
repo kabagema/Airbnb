b36c6784ad583cd263ec643ee84b4b7bafad3b35
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-10 18:03:16 +0200

    router screen

--- App.js
+++ App.js
@@ -5,31 +5,27 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
+
+
 import Post from './src/components/Post';
+
 import DestinationSearchScreen from './src/screens/DestinationSearch';
 import GuestScreen from './src/screens/Guest';
 import HomeScreen from './src/screens/Home';
-
 import SearchResultScreen from './src/screens/SearchResults';
+import Router from './src/navigation/Router';
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        {/* <HomeScreen /> */}
-        {/* <Post post={post1} /> */}
-        {/* <Post />
-        <Post /> */}
-        {/* <SearchResultScreen /> */}
-        {/* <DestinationSearchScreen /> */}
-        <GuestScreen />
-      </SafeAreaView>
+      {/* <Router /> */}
+        <Router />
     </>
   );
 };
-// https://youtu.be/ZxkmTsEYfg8?t=7780
+
 export default App;
