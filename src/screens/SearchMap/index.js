2c4983e89072881a32fb41366f849a53732d734d
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-12 17:38:45 +0200

    second result map

---
+++ src/screens/SearchMap/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import styles from './styles';
+
+import {Text, View} from 'react-native';
+import MapView, {Marker, PROVIDER_DEFAULT} from 'react-native-maps';
+import CustomMarker from '../../components/Marker';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import places from '../../../assets/data/feed';
+
+const SearchMapScreen = (props) => {
+
+    const [PlaceId, setPlaceId] = useState(null);
+
+  return (
+    <View style={styles.MapView}>
+      <MapView
+        style={styles.MapView}
+        provider={PROVIDER_DEFAULT}
+        initialRegion={{
+          latitude: 47.66998,
+          longitude: -122.51247,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}>
+        {places.map(place => (
+          <CustomMarker
+            coordinate={place.coordinate}
+            price={place.newPrice}
+            isSelected={place.id === PlaceId}
+            onPress={() => setPlaceId(place.id)}
+            />
+        ))}
+      </MapView>
+    </View>
+  );
+};
+
+export default SearchMapScreen;
+
---
+++ src/screens/SearchMap/styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  MapView: {
+    width: '100%',
+    height: '100%',
+  },
+  priceContainer: {
+    backgroundColor: 'white',
+    padding: 5,
+    borderRadius: 20,
+    borderColor: 'grey',
+    borderWidth: 1,
+  },
+  price: {
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
