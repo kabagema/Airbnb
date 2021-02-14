f4e49a260b5007fefadb9fae810b1f0bf029b847
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-14 19:12:02 +0200

    buggy but it works

--- src/screens/SearchMap/index.js
+++ src/screens/SearchMap/index.js
@@ -13,13 +13,14 @@ const SearchMapScreen = (props) => {
   const [PlaceId, setPlaceId] = useState(null);
   const width = useWindowDimensions().width;
   const flatList = useRef();
+  const map = useRef();
   const viewConfig = useRef({itemVisiblePercentThreshold: 70});
-  const onViewChange = useRef(({viewableItems}) => {
-    if (viewableItems.length >= 0) {
-      const placeSelected = viewableItems[0].item;
-      setPlaceId(placeSelected.id)
+  const onViewChanged = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setPlaceId(selectedPlace.id);
     }
-  })
+  });
 
   useEffect(() => {
     if (!PlaceId || !flatList) {
@@ -27,18 +28,28 @@ const SearchMapScreen = (props) => {
     }
     const index = places.findIndex((place) => place.id === PlaceId);
     flatList.current.scrollToIndex({index});
-  }, [PlaceId]);
+
+    const selectedPlace = places[index];
+    const region = {
+      latitude: selectedPlace.coordinate.latitude,
+      longitude: selectedPlace.coordinate.longitude,
+      latitudeDelta: 0.1,
+      longitudeDelta: 0.1,
+    };
+    map.current.animateToRegion(region);
+  }, [PlaceId, onViewChanged]);
 
   return (
     <View style={styles.MapView}>
       <MapView
+        ref={map}
         style={styles.MapView}
         provider={PROVIDER_DEFAULT}
         initialRegion={{
           latitude: 47.66998,
           longitude: -122.51247,
-          latitudeDelta: 0.2,
-          longitudeDelta: 0.2,
+          latitudeDelta: 0.3,
+          longitudeDelta: 0.3,
         }}>
         {places.map((place) => (
           <CustomMarker
@@ -50,22 +61,23 @@ const SearchMapScreen = (props) => {
           />
         ))}
       </MapView>
+
       <View style={{position: 'absolute', bottom: 10}}>
         <FlatList
           ref={flatList}
           data={places}
           renderItem={({item}) => <RoundPostItem post={item} />}
           horizontal
+          keyExtractor={places.id}
           showsHorizontalScrollIndicator={false}
-          snapToInterval={width - 60}
+          snapToInterval={width - 70}
           snapToAlignment={'center'}
           decelerationRate={'fast'}
           viewabilityConfig={viewConfig.current}
-          onViewableItemsChanged={onViewChange.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
       </View>
     </View>
   );
 };
-
 export default SearchMapScreen;
