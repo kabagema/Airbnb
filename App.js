443ea771754814df71a1b01c39edacc239ab3d6f
kabagema <35719135+kabagema@users.noreply.github.com>
2021-02-17 15:36:13 +0200

    refactor

--- App.js
+++ App.js
@@ -7,18 +7,11 @@
  */
 import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
 
-import Post from './src/components/Post';
-
-import DestinationSearchScreen from './src/screens/DestinationSearch';
-import GuestScreen from './src/screens/Guest';
-import HomeScreen from './src/screens/Home';
-import SearchResultScreen from './src/screens/SearchResults';
 import Router from './src/navigation/Router';
-import SearchMapScreen from './src/screens/SearchMap';
 
-import {withAuthenticator, signIn, signOut} from 'aws-amplify-react-native';
+import {withAuthenticator} from 'aws-amplify-react-native';
 
 const App: () => React$Node = () => {
   return (
@@ -30,11 +23,3 @@ const App: () => React$Node = () => {
 };
 
 export default withAuthenticator(App);
-
-// https://youtu.be/2zy2qX1eR6E?t=8729
-{
-  /* <Router /> */
-}
-{
-  /* <SearchMapScreen /> */
-}
